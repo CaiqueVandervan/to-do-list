e37147520d60b2156f3abc3667f938e459e44040
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-02-12 02:39:16 -0300

    feat: static temporary logo in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from "next/image"
 
 type LayoutProps = {
     children: React.ReactNode
@@ -6,8 +7,19 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
     return (
-        <main className="w-full h-screen bg-gradient-to-t from-[#0f0c29] via-[#302b63] to-[#4e54c8] flex justify-center items-center">
-            <div>{children}</div>
+        <main className="h-screen bg-gradient-to-t from-[#0f0c29] via-[#302b63] to-[#4e54c8] grid grid-rows-3">
+            <div className="flex items-start justify-center mt-10">
+                <Image
+                    src="/images/logo.svg"
+                    width={400}
+                    height={400}
+                    alt="todolist_logo"
+                />
+            </div>
+            <div className="flex items-center justify-center">
+                {children}
+            </div>
+
         </main>
     )
 }
