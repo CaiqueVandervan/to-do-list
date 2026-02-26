f5da6a41f46f78c1443781c96c681ebae742b55e
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-02-26 02:31:33 -0300

    fix(ui): position element correctly on screen

--- src/components/CustomizedAlert.tsx
+++ src/components/CustomizedAlert.tsx
@@ -4,12 +4,12 @@ type AlertProps = {
     message: React.ReactNode
 }
 
-const Alert = ({message}: AlertProps) => {
+const CustomizedAlert = ({message}: AlertProps) => {
     
     return (
-        <Box className="fixed flex items-center justify-center top-40 rounded-lg h-8 max-w-[750px] px-3 text-white bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#4e54c8] shadow-lg">
+        <Box className="fixed flex items-center justify-center top-[157px] rounded-lg h-8 max-w-[1000px] px-3 text-white bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#4e54c8] shadow-lg">
             {message}
         </Box>
     )
 }
-export default Alert
+export default CustomizedAlert
