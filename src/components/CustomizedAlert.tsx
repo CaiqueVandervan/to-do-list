67a95cf22b1fb7eebf9fc4f5bf0926dcb5eee677
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-02-27 03:02:50 -0300

    feat: add ShinyText component fix(alert): improve CustomizedAlert performance

--- src/components/CustomizedAlert.tsx
+++ src/components/CustomizedAlert.tsx
@@ -1,15 +1,30 @@
-import { Box } from "@mui/material"
+import { Box, Zoom } from "@mui/material"
+import { useEffect, useState } from "react"
 
 type AlertProps = {
     message: React.ReactNode
+    duration: number
+    onClose: () => void
 }
 
-const CustomizedAlert = ({message}: AlertProps) => {
-    
+const CustomizedAlert = ({ message, duration, onClose }: AlertProps) => {
+
+    const [open, setOpen] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() =>
+            setOpen(false), duration)
+
+        return () => clearTimeout(timer)
+    }, [message])
+
     return (
-        <Box className="fixed flex items-center justify-center top-[157px] rounded-lg h-8 max-w-[1000px] px-3 text-white bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#4e54c8] shadow-lg">
-            {message}
-        </Box>
+        <Zoom in={open}
+            onExited={onClose}>
+            <Box className="fixed flex items-center justify-center top-[157px] rounded-lg h-8 max-w-[1000px] px-3 text-white bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#4e54c8] shadow-lg">
+                {message}
+            </Box>
+        </Zoom>
     )
 }
 export default CustomizedAlert
