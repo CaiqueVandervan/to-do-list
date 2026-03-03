a8ab192b1525dea96111f3bd8ed3836b0a9af179
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-03-03 01:53:43 -0300

    refactor: improve code readbility without behavior changes

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,40 +1,6 @@
 import { dataBase } from "@/lib/dataBase";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function editTasks(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "PUT") {
-        try {
-            const { id } = req.query
-            const idNumber = Number(id)
-            const { task_name } = req.body
-            const [resp] = await dataBase.query("UPDATE todolist SET task_name = ? WHERE id = ?", [task_name, idNumber])
-            res.status(200).json(resp)
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    }
-}
-
-export async function concludedTasks(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "PUT") {
-        try {
-            const { id } = req.query
-            const idNumber = Number(id)
-            const { task_name, concluded } = req.body
-
-            const fields: string[] = []
-            const values = []
-
-
-
-            const resp = await dataBase.query("UPDATE tasks SET concluded = ? WHERE id = ?", [concluded, idNumber])
-            res.status(200).json(resp)
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    }
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const { id } = req.query
@@ -48,7 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             res.status(500).json(error)
         }
     }
-
 
     if (req.method === "PUT") {
         try {
