8b92e84c5ddb2fbf5ecfcf18f1035bb767d255a6
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-03-18 23:45:05 -0300

    feat: improve login and register pages with a better layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,82 @@
 import CustomizedButton from "@/components/CustomizedButton"
 import CustomizedTextField from "@/components/CustomizedTextField"
-import Layout from "@/components/Layout"
-import { Card, Box, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import LoginIcon from '@mui/icons-material/Login'
 import CustomizedLink from "@/components/CustomizedLink"
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import Image from "next/image"
 
 const LoginPage = () => {
     return (
-        <Layout>
-            <Card className="p-3">
-                <Box className="h-10 flex justify-center items-start gap-1">
-                    <Typography fontSize={20}>
-                        Login
+        <Box className="h-screen flex items-center justify-center bg-gradient-to-t from-[#0f0c29] via-[#302b63] to-[#4e54c8]">
+
+            <Image className="absolute top-[218px]"
+                src="/images/logo.svg"
+                width={300}
+                height={300}
+                alt="todolist_logo"
+                priority
+            />
+
+            <Box className="flex flex-col items-center gap-4">
+
+                <Box className="p-5 rounded-3xl bg-white">
+
+                    <Box className="h-10 flex justify-center items-center gap-1">
+                        <Typography className="text-gray-700"
+                            fontSize={22}>
+                            Realize o Login e entre com sua conta!
+                        </Typography>
+
+                        <AssignmentIcon className="text-gray-700" />
+                    </Box>
+
+                    <Box className="grid gap-4">
+                        <CustomizedTextField
+                            label="E-mail"
+                            size="medium"
+                            topTitleColor="#9370db"
+                            hoverColor="#4a0072"
+                            borderColor="#4e54c8"
+                            focusColor="#4e54c8" />
+
+                        <CustomizedTextField
+                            label="Senha"
+                            size="medium"
+                            topTitleColor="#9370db"
+                            hoverColor="#4a0072"
+                            borderColor="#4e54c8"
+                            focusColor="#4e54c8" />
+
+                        <CustomizedButton
+                            label="Entrar"
+                            size="small"
+                            variant="contained"
+                            bgColor="#4e54c8"
+                            hoverColor="#302b63"
+                            startIcon={<LoginIcon />} />
+                    </Box>
+
+                </Box>
+
+                <Box className="flex text-white gap-1">
+
+                    <Typography fontSize={16}>
+                        Não possui Conta?
                     </Typography>
-                </Box>
-                <Box className="grid gap-4">
-                    <CustomizedTextField label="E-mail" size="small" topTitleColor="#9370db" hoverColor="#4a0072" borderColor="#4e54c8" focusColor="#4e54c8" className="w-[25rem]" />
-                    <CustomizedTextField label="Senha" size="small" topTitleColor="#9370db" hoverColor="#4a0072" borderColor="#4e54c8" focusColor="#4e54c8" className="w-[25rem]" />
-                </Box>
-                <Box className="h-12 grid grid-cols-2 items-center">
+
                     <CustomizedLink href="http://localhost:3000/register" underline="hover" className="justify-self-start">
-                        <Typography fontSize={14}>
-                            Não possui conta? Cadastre-se
+                        <Typography fontSize={16}>
+                            Cadastre-se
                         </Typography>
                     </CustomizedLink>
-                    <CustomizedButton label="Entrar" size="small" variant="contained" bgColor="#4e54c8" hoverColor="#302b63" className=" justify-self-end w-32" startIcon={<LoginIcon />} />
+
                 </Box>
-            </Card>
-        </Layout>
+
+            </Box>
+
+        </Box>
+
     )
 }
 export default LoginPage
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,73 +1,93 @@
 import CustomizedTextField from "@/components/CustomizedTextField"
-import Layout from "@/components/Layout"
-import { Card, Box, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd'
 import CustomizedButton from "@/components/CustomizedButton";
 import CustomizedLink from "@/components/CustomizedLink";
+import Image from "next/image"
 
 const RegisterPage = () => {
     return (
-        <Layout>
-            <Card className="p-3">
-                <Box className="h-10 flex items-center gap-1 justify-center mb-2">
-                    <Typography
-                        fontSize={20}>
-                        Faça o cadastro para ter sua lista!
+        <Box className="h-screen flex items-center justify-center bg-gradient-to-t from-[#0f0c29] via-[#302b63] to-[#4e54c8]">
+
+            <Image className="absolute top-44"
+                src="/images/logo.svg"
+                width={300}
+                height={300}
+                alt="todolist_logo"
+                priority
+            />
+
+            <Box className="flex flex-col items-center gap-4">
+
+                <Box className="p-5 rounded-3xl bg-white">
+
+                    <Box className="h-10 flex items-center gap-1 justify-center mb-2">
+                        <Typography className="text-gray-700"
+                            fontSize={22}>
+                            Realize o Cadastro para ter sua lista!
+                        </Typography>
+
+                        <AssignmentIcon className="text-gray-700" />
+                    </Box>
+
+                    <form className="grid gap-4"
+                        onSubmit={() => alert("bla")}>
+                        <CustomizedTextField
+                            label="Nome de usuário"
+                            size="medium"
+                            topTitleColor="#9370db"
+                            hoverColor="#4a0072"
+                            borderColor="#4e54c8"
+                            focusColor="#4e54c8" />
+
+                        <CustomizedTextField
+                            label="Seu melhor e-mail"
+                            size="medium"
+                            topTitleColor="#9370db"
+                            hoverColor="#4a0072"
+                            borderColor="#4e54c8"
+                            focusColor="#4e54c8" />
+
+                        <CustomizedTextField
+                            label="Senha forte"
+                            size="medium"
+                            topTitleColor="#9370db"
+                            hoverColor="#4a0072"
+                            borderColor="#4e54c8"
+                            focusColor="#4e54c8" />
+
+                        <CustomizedButton
+                            label="Cadastrar"
+                            size="small"
+                            variant="contained"
+                            bgColor="#4e54c8"
+                            hoverColor="#302b63"
+                            startIcon={<AssignmentIndIcon />}
+                            type="submit" />
+                    </form>
+
+                </Box>
+
+                <Box className="flex gap-1 text-white">
+                    <Typography fontSize={16}>
+                        Ja possui conta?
                     </Typography>
-
-                    <AssignmentIcon className="text-gray-700" />
-                </Box>
-
-                <Box className="grid gap-4">
-                    <CustomizedTextField className="w-[25rem]"
-                        label="Nome de usuário"
-                        size="small"
-                        topTitleColor="#9370db"
-                        hoverColor="#4a0072"
-                        borderColor="#4e54c8"
-                        focusColor="#4e54c8" />
-
-                    <CustomizedTextField className="w-[25rem]"
-                        label="Seu melhor e-mail"
-                        size="small"
-                        topTitleColor="#9370db"
-                        hoverColor="#4a0072"
-                        borderColor="#4e54c8"
-                        focusColor="#4e54c8" />
-
-                    <CustomizedTextField className="w-[25rem]"
-                        label="Senha forte"
-                        size="small"
-                        topTitleColor="#9370db"
-                        hoverColor="#4a0072"
-                        borderColor="#4e54c8"
-                        focusColor="#4e54c8" />
-                </Box>
-
-                <Box className="h-12 grid grid-cols-2 items-center">
                     <CustomizedLink className="justify-self-start"
                         href="http://localhost:3000/login"
                         underline="hover" >
 
-                        <Typography
-                            fontSize={14}>
-                            Ja possui conta? Faça login
+                        <Typography fontSize={16}>
+                            Faça login
                         </Typography>
 
                     </CustomizedLink>
-
-                    <CustomizedButton className="justify-self-end w-32"
-                        label="Cadastrar"
-                        size="small"
-                        variant="contained"
-                        bgColor="#4e54c8"
-                        hoverColor="#302b63"
-                        startIcon={<AssignmentIndIcon />} />
                 </Box>
 
-            </Card>
-        </Layout>
+            </Box>
+
+        </Box>
+
     )
 }
 export default RegisterPage
